ecb3478e9f3b5634426f1fc3ba08b57fa724bd53
JorgeCal123/boom.git <git@github.com>
2022-02-11 07:06:48 -0800

    punto 1 description

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort
+ * algorithm
+ * @list: is a list type listint_t
+ */
+void insertion_sort_list(listint_t **list)a
+{
+
+}
