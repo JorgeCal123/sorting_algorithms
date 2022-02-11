afd5d07f395199941435f61bc5565f77e2539739
jhonpulido1990 <3825@holbertonschool.com>
2022-02-11 10:08:14 -0500

    sort.h

---
+++ sort.h
@@ -0,0 +1,76 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*include library*/
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list)
+{
+    int i;
+
+    i = 0;
+    while (list)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", list->n);
+        ++i;
+        list = list->next;
+    }
+    printf("\n");
+}
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/*function that sorts an array of integers in
+ascending order using the Bubble sort algorithm*/
+void bubble_sort(int *array, size_t size);
+/*function that sorts a doubly linked list of integers
+in ascending order using the Insertion sort algorithm*/
+void insertion_sort_list(listint_t **list);
+/*Write a function that sorts an array of integers in
+ascending order using the Selection sort algorithm*/
+void selection_sort(int *array, size_t size);
+/*Write a function that sorts an array of integers in
+ascending order using the Quick sort algorithm*/
+void quick_sort(int *array, size_t size);
+
+#endif /* MAIN_H */
