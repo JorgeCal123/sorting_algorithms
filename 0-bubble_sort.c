529b1b767d757b58ba4853409a820c6616a606da
JorgeCal123/boom.git <git@github.com>
2022-02-11 06:58:30 -0800

    punto 0 descripcion

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,2 +1,11 @@
 #include "sort.h"
+/**
+ * bubble_sort - function that sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: list with number of type integer
+ * @size: size of list
+ */
+void bubble_sort(int *array, size_t size)
+{
 
+}
