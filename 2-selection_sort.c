b0d3c53b41d7571d0f49fdb9da58562456708cd5
jhonpulido1990 <3825@holbertonschool.com>
2022-02-12 14:24:52 -0500

    correction output task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,9 +19,13 @@ void selection_sort(int *array, size_t size)
 			{
 				min_idx = j;
 			}
+		}
+		if (i != min_idx)
+		{
 			tmp = array[min_idx];
 			array[min_idx] = array[i];
 			array[i] = tmp;
+			print_array(array, size);
 		}
 	}
 }
