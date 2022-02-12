51d1cd0954d98e6c8252b9a1a9472f962f0e0e02
JorgeCal123/boom.git <git@github.com>
2022-02-12 07:12:02 -0800

    punto 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,5 +7,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	
+    listint_t *i = (*list)->next;
+    listint_t *j = NULL;
+    listint_t *key = NULL;
+
+    while (i)
+    {
+
+        key = i;
+        j = i->prev;
+        if((*list)->prev != NULL)
+          (*list) = (*list)->prev;
+        while(j->n > key->n)
+        {
+            j->next = i->next;
+            if (i->next != NULL)
+                i->next->prev = j;
+            i->next = j;
+            i->prev = j->prev;
+            if (j->prev != NULL)
+                j->prev->next = i;
+            j->prev = i;
+
+            j = j->prev; 
+            if (j->prev != NULL)
+              i = i->prev; 
+
+        }
+      i = (i)->next;
+    }
 }
